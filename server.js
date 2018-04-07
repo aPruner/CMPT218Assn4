6d5bf7699aecb416d3b4f976e8df1f8808b5c202
vberezny <v.bereznyakov@gmail.com>
2018-04-07 16:10:15 -0700

    passport stuff so far

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 
 var port = process.env.PORT || 3000;
 
@@ -11,6 +13,24 @@ var options = {
   extensions: ['htm','html'],
   index: "index.html"
 };
+
+//setup passport strategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    uModel.findOne({uname: username}, function (err, user){
+      if (err) { return done(err); }
+      //Incorrect user name
+      if (!user) {
+        return done(null, false);
+      }
+      //Incorrect password
+      if (!user.verifyPassword(password)) {
+        return done(null, false);
+      }
+      return done(null,user);
+    });
+  }
+));
 
 //setup mongoose connection
 mongoose.connect("mongodb://assignment4:assignment4@ds123399.mlab.com:23399/a3db");
